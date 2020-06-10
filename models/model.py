3bfab706bb9574e1c703ff41d14c418826596e11
zhoujun <572459439@qq.com>
2020-06-10 10:27:15 +0800

    修复resnest50的bug

--- models/model.py
+++ models/model.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     x = torch.zeros(2, 3, 640, 640).to(device)
 
     model_config = {
-        'backbone': {'type': 'shufflenet_v2_x0_5', 'pretrained': True, "in_channels": 3},
+        'backbone': {'type': 'resnest50', 'pretrained': True, "in_channels": 3},
         'neck': {'type': 'FPN', 'inner_channels': 256},  # 分割头，FPN or FPEM_FFM
         'head': {'type': 'DBHead', 'out_channels': 2, 'k': 50},
     }
